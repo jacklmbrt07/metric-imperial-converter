c1eb78ccedd06c52eb3cef7635a5da4709d0f66d
jacklmbrt07 <jacklmbrt07@gmail.com>
2020-12-14 16:20:22 -0800

    pass all tests except fcc

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,7 +24,7 @@ suite("Functional Tests", function () {
           .end(function (err, res) {
             assert.equal(res.status, 200);
             assert.equal(res.body.initNum, 10);
-            assert.equal(res.body.initUnit, "l");
+            assert.equal(res.body.initUnit, "L");
             assert.approximately(res.body.returnNum, 2.64172, 0.1);
             assert.equal(res.body.returnUnit, "gal");
             done();
